e9f5c71a06344b3ac2d864c694ad936c6ddb85bb
jay91537 <veuxft@gmail.com>
2024-11-25 14:12:31 +0900

    Feat : 16주차 완료

--- noeyeyh/Week4/팰린드롬수.py
+++
@@ -1,8 +0,0 @@
-while True:
-  number = input().strip()
-  if number == '0':
-    break
-  elif number == number[::-1]:
-    print("yes")
-  else:
-    print("no")
--- noeyeyh/Week6/평균.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-l = list(map(int, input(). split()))
-
-s = sum(l)
-m = max(l)
-average = ((100 * s) / m / n)
-print(average)
