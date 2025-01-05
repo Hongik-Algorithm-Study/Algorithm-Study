7624f994edadbe4acb17ed3ce69c9de622e46a61
이예지 <oyatpeach@naver.com>
2025-01-05 18:33:34 +0900

    Feat : 19주차_폰켓몬 완료

---
+++ oyatplum/Week19/폰켓몬.py
@@ -0,0 +1,11 @@
+def solution(nums):
+    answer = 0
+    maxLen = len(set(nums))
+    numLen = len(nums)//2
+
+    if maxLen < numLen:
+        answer = maxLen
+    else:
+        answer = numLen
+
+    return answer
