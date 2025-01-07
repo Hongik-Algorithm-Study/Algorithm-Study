8032691356085c183dbab5fff640c016830ea39d
jay91537 <veuxft@gmail.com>
2025-01-07 20:28:45 +0900

    Feat : 19주차_완주하지 못한 선수 완료

---
+++ jay91537/Week19/완주하지 못한 선수.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap <String, Integer> completeMan = new HashMap<>();
+
+        ArrayList<String> participantMan = new ArrayList<>();
+
+        for (int i = 0; i < completion.length; i++) {
+            if(completeMan.containsKey(completion[i])){
+                completeMan.put(completion[i], completeMan.get(completion[i])+1);
+            } else {
+                completeMan.put(completion[i], 1);
+            }
+        }
+
+        participantMan.addAll(Arrays.asList(participant));
+
+        String ans = null;
+        for (String person : participant) {
+            if (completeMan.containsKey(person) && completeMan.get(person) > 0) {
+                completeMan.put(person, completeMan.get(person) - 1);
+            } else {
+                ans = person;
+                break;
+            }
+        }
+        return ans;
+    }
+}
