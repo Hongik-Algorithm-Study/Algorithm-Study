34669e68b4d6b94a64ea80a4b07ce09e4788e0b0
jay91537 <veuxft@gmail.com>
2025-01-13 00:47:00 +0900

    Feat : 20주차_같은 숫자는 싫어 완료

---
+++ jay91537/Week20/같은 숫자는 싫어.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public Queue<Integer> solution(int []arr) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(arr[0]);
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i-1]!=arr[i]) {
+                queue.add(arr[i]);
+            }
+        }
+
+        return queue;
+    }
+}
