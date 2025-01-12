1aa8921cc2c4ce95f1b369f0a0f95690a7ee3c7d
ity0526 <ity0526@gmail.com>
2025-01-12 22:24:01 +0900

    Feat : 20주차_올바른 괄호 완료

---
+++ ity0562/ity0526_week20/올바른 괄호.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def solution(s):
+    answer = True
+    open = deque()
+
+    for i in s:
+        if i == "(":
+            open.append(i)
+
+        else:
+            if open:
+                open.pop()
+
+            else:
+                answer = False
+                break
+
+    if not open:
+        answer = False
+
+    return answer
